dde987b880712b0ec9db24e16e3138b7ff350ba7
gererock <1rochi9gere@gmail.com>
2026-04-06 16:41:34 -0300

    ej5 terminado

--- src/main/java/com/programacion4/unidad4ej5/config/exceptions/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.programacion4.unidad4ej5.config.exceptions;
-
-public class ResourceNotFoundException extends RuntimeException {
-
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
